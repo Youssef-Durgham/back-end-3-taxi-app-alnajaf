2f0abbd8d73c5c24041e643ecc6de3529f3c5a7e
Youssef Durgham <73782996+Youssef-Durgham@users.noreply.github.com>
2023-06-05 01:48:21 +0300

    Update Home.js

--- routes/Home.js
+++ routes/Home.js
@@ -150,38 +150,50 @@ let captainClients = new Map(); // {captainId: ws}
 let userClients = new Map(); // {userId: ws}
 let adminClients = []; // List of admin clients
 wss.on('connection', ws => {
-console.log(captainClients, userClients);
-ws.send(JSON.stringify({ message: 'Connection successful!' }));
-
-ws.on('message', message => {
+  console.log(captainClients, userClients);
+  ws.send(JSON.stringify({ message: 'Connection successful!' }));
+  
+  ws.on('message', message => {
   let payload = JSON.parse(message);
   const { token } = payload;
   const { role, id } = jwt.verify(token, process.env.JWT_SECRET); // Verify the JWT token
-console.log(token, role, id)
-console.log(captainClients, userClients)
-console.log(payload)
+  console.log(token, role, id)
+  console.log(captainClients, userClients)
+  console.log(payload)
 // Check role to determine who is connecting (captain, user, or admin)
 if (role === 'captain') {
   captainClients.set(id, ws);
-  // The following block is executed whenever a message is received from the captain
+
+  // Update location if provided
   if (payload.location) {
     ws.location = payload.location;
-    // ... notify admin clients ...
-    TaxiOrder.find({ captain: id, cancelled: false }).sort('-createdAt')
-    .limit(1)
-    .exec((err, orders) => {
-      if (err) return console.error(err);
-      if (orders.length === 0) return;
-      let order = orders[0];
-      let userId = order.user;
-      // Check if the user has an active order with this captain
-      if (userClients.has(userId)) {
-        // Send the location to the user
-        sendLocationToUser(userId, id, ws.location);
+
+    // Notify all admin clients about this captain's location
+    adminClients.forEach(adminWs => {
+      if (adminWs.readyState === WebSocket.OPEN) {
+        adminWs.send(JSON.stringify({ captainId: id, location: ws.location }));
       }
     });
-  }
 
+    // Notify the main user and all passenger users with a recent order associated with this captain
+    TaxiOrder.find({ captain: id, cancelled: false }).sort('-createdAt')
+      .limit(1)
+      .exec((err, orders) => {
+        if (err) return console.error(err);
+        if (orders.length === 0) return;
+
+        let order = orders[0];
+
+        // Send location to main user
+        let userId = order.user;
+        sendLocationToUser(userId, id, ws.location);
+
+        // Send location to all passengers
+        order.passengers.forEach(passenger => {
+          sendLocationToUser(passenger.user, id, ws.location);
+        });
+      });
+  }
 } else if (role === 'user') {
   userClients.set(id, ws);
   ws.send(JSON.stringify({ message: 'User added successfully!' }));
@@ -204,6 +216,7 @@ if (role === 'captain') {
         ws.send(JSON.stringify({ captainId: captainId, location: captainWs.location }));
       }
     });
+
 } else if (role === 'admin') {
   adminClients.push(ws);
 
@@ -215,42 +228,36 @@ if (role === 'captain') {
     }
   }
   ws.send(JSON.stringify(locations));
-}
-});
+}});
 
-ws.on('close', () => {
-for (let [captainId, captainWs] of captainClients.entries()) {
-  if (ws === captainWs) {
-      captainClients.delete(captainId);
-  }
-}
+  ws.on('close', () => {
+      for (let [captainId, captainWs] of captainClients.entries()) {
+          if (ws === captainWs) {
+              captainClients.delete(captainId);
+          }
+      }
 
-for (let [userId, userWs] of userClients.entries()) {
-  if (ws === userWs) {
-      userClients.delete(userId);
-  }
-}
+      for (let [userId, userWs] of userClients.entries()) {
+          if (ws === userWs) {
+              userClients.delete(userId);
+          }
+      }
 
-let adminIndex = adminClients.indexOf(ws);
-if (adminIndex !== -1) {
-    adminClients.splice(adminIndex, 1);
-}
-});
+      let adminIndex = adminClients.indexOf(ws);
+      if (adminIndex !== -1) {
+          adminClients.splice(adminIndex, 1);
+      }
+  });
 });
 
 // Helper function to send location to a user
 function sendLocationToUser(userId, captainId, location) {
-let userWs = userClients.get(userId.toString());
+  let userWs = userClients.get(userId.toString());
 
-// If the user's WebSocket is open, send the captain's location to the user
-if (userWs && userWs.readyState === WebSocket.OPEN) {
-  userWs.send(JSON.stringify({
-    captainId: captainId,
-    location: location
-  }));
-} else {
-  // The user's WebSocket is not open, so do nothing
-}
+  // If the user's WebSocket is open, send the captain's location to the user
+
+    userWs.send(JSON.stringify({ captainId: captainId, location: location }));
+  
 }
 
 
