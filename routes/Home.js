78da27a539350b833cc6bccea7ff55ea45bdb07f
Youssef Durgham <73782996+Youssef-Durgham@users.noreply.github.com>
2023-06-04 23:31:05 +0300

    Update Home.js

--- routes/Home.js
+++ routes/Home.js
@@ -150,16 +150,16 @@ let captainClients = new Map(); // {captainId: ws}
 let userClients = new Map(); // {userId: ws}
 let adminClients = []; // List of admin clients
 wss.on('connection', ws => {
-  console.log(captainClients, userClients);
-  ws.send(JSON.stringify({ message: 'Connection successful!' }));
-  
-  ws.on('message', message => {
-  let payload = JSON.parse(message);
-  const { token } = payload;
-  const { role, id } = jwt.verify(token, process.env.JWT_SECRET); // Verify the JWT token
-  console.log(token, role, id)
-  console.log(captainClients, userClients)
-  console.log(payload)
+console.log(captainClients, userClients);
+ws.send(JSON.stringify({ message: 'Connection successful!' }));
+
+ws.on('message', message => {
+let payload = JSON.parse(message);
+const { token } = payload;
+const { role, id } = jwt.verify(token, process.env.JWT_SECRET); // Verify the JWT token
+console.log(token, role, id)
+console.log(captainClients, userClients)
+console.log(payload)
 // Check role to determine who is connecting (captain, user, or admin)
 if (role === 'captain') {
   captainClients.set(id, ws);
@@ -213,7 +213,9 @@ if (role === 'captain') {
 
       // If the captain's WebSocket is open and the location is known, send the location to the user
       if (captainWs && captainWs.readyState === WebSocket.OPEN && captainWs.location) {
-        ws.send(JSON.stringify({ captainId: captainId, location: captainWs.location }));
+        while (captainWs.readyState === WebSocket.OPEN && captainWs.location) {
+          ws.send(JSON.stringify({ captainId: captainId, location: captainWs.location }));
+        }
       }
     });
 
@@ -228,38 +230,39 @@ if (role === 'captain') {
     }
   }
   ws.send(JSON.stringify(locations));
-}});
+}
+});
 
-  ws.on('close', () => {
-      for (let [captainId, captainWs] of captainClients.entries()) {
-          if (ws === captainWs) {
-              captainClients.delete(captainId);
-          }
-      }
+ws.on('close', () => {
+for (let [captainId, captainWs] of captainClients.entries()) {
+  if (ws === captainWs) {
+      captainClients.delete(captainId);
+  }
+}
 
-      for (let [userId, userWs] of userClients.entries()) {
-          if (ws === userWs) {
-              userClients.delete(userId);
-          }
-      }
+for (let [userId, userWs] of userClients.entries()) {
+  if (ws === userWs) {
+      userClients.delete(userId);
+  }
+}
 
-      let adminIndex = adminClients.indexOf(ws);
-      if (adminIndex !== -1) {
-          adminClients.splice(adminIndex, 1);
-      }
-  });
+let adminIndex = adminClients.indexOf(ws);
+if (adminIndex !== -1) {
+    adminClients.splice(adminIndex, 1);
+}
+});
 });
 
 // Helper function to send location to a user
 function sendLocationToUser(userId, captainId, location) {
-  let userWs = userClients.get(userId.toString());
+let userWs = userClients.get(userId.toString());
 
-  // If the user's WebSocket is open, send the captain's location to the user
-  if (userWs && userWs.readyState === WebSocket.OPEN) {
-    userWs.send(JSON.stringify({ captainId: captainId, location: location }));
-  } else {
-    // The user's WebSocket is not open, so do nothing
-  }
+// If the user's WebSocket is open, send the captain's location to the user
+if (userWs && userWs.readyState === WebSocket.OPEN) {
+userWs.send(JSON.stringify({ captainId: captainId, location: location }));
+} else {
+// The user's WebSocket is not open, so do nothing
+}
 }
 
 
